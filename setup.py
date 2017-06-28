0a484f25ffaa0e034dfca4c1ab1da1ed32484e13
SolarFresh <shangyuhuang@gmail.com>
2017-06-28 23:37:22 +0800

    Initi Commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='SolarImage',
+    version='0.0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='SolarFresh',
+    author_email='shangyuhuang@gmail.com',
+    description=''
+)
