93a55bf4d7a20d2bd1e199b43c2c41f01d70cec7
SolarFresh <shangyuhuang@gmail.com>
2017-07-02 21:47:35 +0800

    size of images should be a parameter as input

--- SolarImage/classifier/nn/model/regression.py
+++ SolarImage/classifier/nn/model/regression.py
@@ -1,19 +1,17 @@
 from tensorflow import Variable
-from tensorflow import float32
 from tensorflow import matmul
-from tensorflow import placeholder
 from tensorflow import zeros
 
 
-def linear(D, K):
+def linear(x, d, k):
     """
-    The image x has all of its pixels flattened out to a single column vector of shape [D x 1].
-    The matrix W (of size [K x D]), and the vector b (of size [K x 1]) are the parameters of the function.
-    :param D:  size of image x in a single column vector
-    :param K:  size of label to be classified
+    The image x has all of its pixels flattened out to a single column vector of shape [d x 1].
+    The matrix W (of size [k x d]), and the vector b (of size [k x 1]) are the parameters of the function.
+    :param x:  placeholder of a column vector as an input with the dimension D
+    :param d:  size of image x in a single column vector
+    :param k:  size of label to be classified
     :return:
     """
-    x = placeholder(float32, [None, D])
-    W = Variable(zeros([D, K]))
-    b = Variable(zeros([K]))
-    return matmul(x, W) + b
+    w = Variable(zeros([d, k]))
+    b = Variable(zeros([k]))
+    return matmul(x, w) + b
