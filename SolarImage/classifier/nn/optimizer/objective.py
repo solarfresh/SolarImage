c396d1dc0c27ee90b62eb352fcb77c804c1b5f3e
SolarFresh <shangyuhuang@gmail.com>
2017-07-02 17:59:17 +0800

    target function should a parameter passed from iterator

--- SolarImage/classifier/nn/optimizer/objective.py
+++ SolarImage/classifier/nn/optimizer/objective.py
@@ -1,11 +1,9 @@
 from tensorflow import Tensor
-from tensorflow import float32
 from tensorflow import nn
-from tensorflow import placeholder
 from tensorflow import reduce_mean
 
 
-def cross_entropy(y):
+def cross_entropy(y, y_):
     """
     The raw formulation of cross-entropy
         tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(tf.nn.softmax(y)),
@@ -13,10 +11,12 @@ def cross_entropy(y):
     can be numerically unstable.
     So here we use tf.nn.softmax_cross_entropy_with_logits on the raw
     :param y: the output vector results from nn
+    :param y_: the target vector an optimizer tends to achieve
     :return: average loss between target y' and training vector y
     """
     if not isinstance(y, Tensor):
-        raise TypeError("The type of input musr be tf.Tensor.")
-    y_ = placeholder(float32, y.shape)
+        raise TypeError("The type of y musr be tf.Tensor.")
+    if not isinstance(y_, Tensor):
+        raise TypeError("The type of y_ musr be tf.Tensor.")
     return reduce_mean(
-      nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
+        nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
