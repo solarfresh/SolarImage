56e6faaec2f64bf12cbe2607becba4ae21da02b2
SolarFresh <shangyuhuang@gmail.com>
2017-07-04 23:58:58 +0800

    use a file to pack the process of training

---
+++ solarimage/classifier/nn/optimizer/stepper.py
@@ -0,0 +1,13 @@
+from tensorflow import train
+
+
+def gradient_decent(learning_rate, objective):
+    """
+     This is a first-order iterative optimization algorithm for finding the minimum of a function.
+     To find a local minimum of a function using gradient descent, one takes steps proportional to
+     the negative of the gradient (or of the approximate gradient) of the function at the current point.
+    :param learning_rate: A Tensor or a floating point value. The learning rate to use.
+    :param objective: An objective function used to be minimized
+    :return: An Operation that updates the variables
+    """
+    return train.GradientDescentOptimizer(learning_rate).minimize(objective)
