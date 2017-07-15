cac7492b39e9f3d0663ba174c68caf2a27f162da
SolarFresh <shangyuhuang@gmail.com>
2017-07-15 14:16:17 +0800

    add model of generative adversarial net

--- solarimage/classifier/nn/model/regression.py
+++ solarimage/classifier/nn/model/regression.py
@@ -1,4 +1,17 @@
 from tensorflow import matmul
+from tensorflow import nn
+
+
+def genradvers(x, w1, b1, w2, b2):
+    """
+    Here, we collect models of generative adversarial nets
+    :return:
+    """
+    h1 = nn.relu(matmul(x, w1) + b1)
+    #  Be not sure the movitation of using the second regression
+    #  It is necessary to provide an reason later.
+    log_prob = matmul(h1, w2) + b2
+    return nn.sigmoid(log_prob)
 
 
 def linear(x, w, b):
